1507f8ad428b5ebb0e153a437d3d21063e79c9fe
FuturoDevTeu <mateusnunes8510@gmail.com>
2025-11-11 10:58:43 -0300

    adicionado login

--- back/auth_routes.py
+++ back/auth_routes.py
@@ -24,3 +24,17 @@ async def criar_conta(usuario_schemas: UsuarioSchemas, session: Session = Depend
         session.commit()
         return {"mensagem":"usuario cadastrado com sucesso"}
 
+@auth_router.post("/logar")
+async def logar(usuario_schemas: UsuarioSchemas, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.login == usuario_schemas.login).first()
+    
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Login não encontrado")
+    
+    senha_valida = bcrypt_context.verify(usuario_schemas.senha, usuario.senha)
+    if not senha_valida:
+        raise HTTPException(status_code=401, detail="Senha incorreta")
+    
+    return {"mensagem": "login realizado com sucesso ", "usuario":usuario.login}
+
+
